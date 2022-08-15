5e776cb74c3a2d546103a67168b29ce9fb2e72a3
Varsha-gl <109643864+Varsha-gl@users.noreply.github.com>
2022-08-15 21:00:45 +0530

    Add files via upload

---
+++ TickectRaising/tickectdata.js
@@ -0,0 +1,48 @@
+const data=[
+    {
+        id:0,
+        timestamp:"10:00 AM",
+        priority:1,
+        ticketdesc:"Mys to Blore",
+        ticketstatus:"confirmed",
+        summary:"closed"
+    
+
+    
+    },
+    {
+        id:1,
+        timestamp:"10:00 AM",
+        priority:3,
+        ticketdesc:"Mys to Blore",
+        ticketstatus:"confirmed",
+        summary:"opened"
+    
+    },
+    {
+        id:2,
+        timestamp:"1:00 PM",
+        priority:5,
+        ticketdesc:"Mys to Blore",
+        ticketstatus:"Not-confirmed",
+        summary:"opened"
+    },
+    {
+        id:3,
+        timestamp:"10:00 AM",
+        priority:2,
+        ticketdesc:"Mys to Blore",
+        ticketstatus:"Not-confirmed",
+        summary:"closed"
+    },
+    {
+        id:4,
+        timestamp:"11:00 AM",
+        priority:2,
+        ticketdesc:"Mys to Blore",
+        ticketstatus:"confirmed", 
+        summary:"closed"
+    },
+
+]
+export default data;
